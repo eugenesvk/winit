1382a0aa1a3d5aca1447fc527c0b8cf3bc7288b6
eugenesvk <eugenesvk@users.noreply.github.com>
2025-01-30 20:53:02 +0700

    Windows: add a separate way to set titlebar
    set WS_CAPTION to allow implementing custom titlebar while maintaining resize borders.
    
    Currently MARKER_DECORATIONS internal window flag is used for both WS_CAPTION and WS_BORDER

--- src/platform_impl/windows/window_state.rs
+++ src/platform_impl/windows/window_state.rs
@@ -123,6 +123,8 @@ bitflags! {
 
         const CLIP_CHILDREN = 1 << 22;
 
+        const TITLE_BAR = 1 << 23; //0x__800000 on Windows sets WS_CAPTION (0x__C00000)
+
         const EXCLUSIVE_FULLSCREEN_OR_MASK = WindowFlags::ALWAYS_ON_TOP.bits();
     }
 }
@@ -286,6 +288,9 @@ impl WindowFlags {
                 style &= !(WS_CAPTION | WS_BORDER);
                 style_ex &= !WS_EX_WINDOWEDGE;
             }
+            if !self.contains(WindowFlags::TITLE_BAR) {
+                style &= !WS_CAPTION;
+            }
         }
         if self.contains(WindowFlags::POPUP) {
             style |= WS_POPUP;
@@ -301,6 +306,11 @@ impl WindowFlags {
         }
         if self.contains(WindowFlags::CLIP_CHILDREN) {
             style |= WS_CLIPCHILDREN;
+        }
+        if self.contains(WindowFlags::TITLE_BAR) {
+            style |= WS_CAPTION;
+        } else {
+            style &= !WS_CAPTION;
         }
 
         if self.intersects(
@@ -433,6 +443,9 @@ impl WindowFlags {
             // `WM_NCCALCSIZE`.
             if !self.contains(WindowFlags::MARKER_DECORATIONS) {
                 style &= !(WS_CAPTION | WS_SIZEBOX);
+            }
+            if !self.contains(WindowFlags::TITLE_BAR) {
+                style &= !WS_CAPTION;
             }
 
             util::win_to_err({
