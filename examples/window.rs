58de4747b485ad872e92d148640af412c48426be
eugenesvk <eugenesvk@users.noreply.github.com>
2025-05-18 00:17:16 +0700

    add kbd print to window

--- examples/window.rs
+++ examples/window.rs
@@ -19,6 +19,71 @@ struct App {
     window: Option<Box<dyn Window>>,
 }
 
+
+use winit::event::{Modifiers, KeyEvent};
+// pub struct Modifiers { // CORE: Describes keyboard modifiers event.
+  //   state       : ModifiersState,
+  //   pressed_mods: ModifiersKeys , // NOTE: Currently pressed modifiers keys. The field providing a metadata, it shouldn't be used as a source of truth.
+  // impl Modifiers {
+  //   fn lshift_state(&self) -> ModifiersKeyState { /// The state of the left shift key
+  //     self.mod_state(ModifiersKeys::LSHIFT)}
+pub fn mod_state_logic_s(state:&ModifiersState) -> String {
+  let mut s = String::new();
+  if state.contains(ModifiersState::SHIFT  ){s.push('⇧')}else{s.push(' ')};
+  if state.contains(ModifiersState::CONTROL){s.push('⎈')}else{s.push(' ')};
+  if state.contains(ModifiersState::META   ){s.push('◆')}else{s.push(' ')};
+  if state.contains(ModifiersState::ALT    ){s.push('⎇')}else{s.push(' ')};
+  s
+}
+pub fn mod_state_phys_s(mods:&Modifiers) -> String {
+  let mut s = String::new();
+  if let ModifiersKeyState::Pressed = mods.lshift_state()     {s.push_str("‹⇧ ")}else{s.push_str("  ")};
+  if let ModifiersKeyState::Pressed = mods.rshift_state()     {s.push_str(" ⇧›")}else{s.push_str("  ")};
+  if let ModifiersKeyState::Pressed = mods.lcontrol_state()   {s.push_str("‹⎈ ")}else{s.push_str("  ")};
+  if let ModifiersKeyState::Pressed = mods.rcontrol_state()   {s.push_str(" ⎈›")}else{s.push_str("  ")};
+  if let ModifiersKeyState::Pressed = mods.lmeta_state()      {s.push_str("‹◆ ")}else{s.push_str("  ")};
+  if let ModifiersKeyState::Pressed = mods.rmeta_state()      {s.push_str(" ◆›")}else{s.push_str("  ")};
+  if let ModifiersKeyState::Pressed = mods.lalt_state()       {s.push_str("‹⎇ ")}else{s.push_str("  ")};
+  if let ModifiersKeyState::Pressed = mods.ralt_state()       {s.push_str(" ⎇›")}else{s.push_str("  ")};
+  s
+}
+  // pub struct KeyEvent {
+    // pub physical_key: PhysicalKey, enum PhysicalKey
+      // Code(KeyCode)
+      // �Unidentified(NativeKeyCode),
+    // pub logical_key: Key,// pub enum Key<Str = SmolStr> {
+      // Named(NamedKey),
+      // Character(Str),
+      // �Unidentified(NativeKey),
+      // 🕱Dead(Option<char>),
+    // pub text: Option<SmolStr>,
+    // pub location: KeyLocation, pub enum KeyLocation { Standard,Left,Right,Numpad,
+    // pub state: ElementState, pressed/released
+    // pub repeat: bool, 🔁
+use winit::keyboard::{PhysicalKey, Key, ModifiersState, ModifiersKeyState, KeyLocation};
+use winit::event::ElementState;
+pub fn ev_key_s(key:&KeyEvent) -> String {
+  let mut s = String::new();
+  match &key.state {
+    ElementState::Pressed   => {s.push('↓')},
+    ElementState::Released  => {s.push('↑')},
+  }
+  if key.repeat {s.push('🔁')}else{s.push(' ')}; //𜱣⚛
+  if let PhysicalKey    ::Code        (key_code         ) = &key.physical_key   {s.push_str(&format!("{:?} "    ,key_code           ))};
+  if let PhysicalKey    ::Unidentified(key_code_native  ) = &key.physical_key   {s.push_str(&format!("�{:?} "   ,key_code_native    ))};
+  if let Key            ::Named       (key_named        ) = &key.logical_key    {s.push_str(&format!("{:?} "    ,key_named          ))};
+  if let Key            ::Character   (key_char         ) = &key.logical_key    {s.push_str(&format!("{} "      ,key_char           ))};
+  if let Key            ::Unidentified(key_native       ) = &key.logical_key    {s.push_str(&format!("�{:?} "   ,key_native         ))};
+  if let Key            ::Dead        (maybe_char       ) = &key.logical_key    {s.push_str(&format!("🕱{:?} "   ,maybe_char         ))};
+  match &key.location {
+    KeyLocation::Standard   => {s.push('≝')},
+    KeyLocation::Left       => {s.push('←')},
+    KeyLocation::Right      => {s.push('→')},
+    KeyLocation::Numpad     => {s.push('🔢')},
+  }
+  s
+}
+
 impl ApplicationHandler for App {
     fn can_create_surfaces(&mut self, event_loop: &dyn ActiveEventLoop) {
         #[cfg(not(web_platform))]
@@ -37,10 +102,23 @@ impl ApplicationHandler for App {
     }
 
     fn window_event(&mut self, event_loop: &dyn ActiveEventLoop, _: WindowId, event: WindowEvent) {
-        println!("{event:?}");
         match event {
+            WindowEvent::ModifiersChanged(mods) => {
+                let state       = mods.state() ;//ModifiersState;
+                // let pressed_mods= mods.ModifiersKeys ; // NOTE: Currently pressed modifiers keys. The field providing a metadata, it shouldn't be used as a source of truth.
+                let state_s = mod_state_logic_s(&state);
+                let pressed_mods_s = mod_state_phys_s(&mods);
+                println!("Δ\t{}\n\t{} phys↓",state_s, pressed_mods_s);
+                // ModifiersChanged(Modifiers { state: ModifiersState(ALT), pressed_mods: ModifiersKeys(0x0) })
+                // window.modifiers = modifiers.state();
+                // info!("Modifiers changed to {:?}", window.modifiers);
+            },
+            WindowEvent::KeyboardInput { event, is_synthetic, .. } => {
+                let key_event_s = ev_key_s(&event);
+                let is_synthetic_s = if is_synthetic{"⚗"}else{" "};
+                println!("{}{}",is_synthetic_s,key_event_s);
+            },
             WindowEvent::CloseRequested => {
-                println!("Close was requested; stopping");
                 event_loop.exit();
             },
             WindowEvent::SurfaceResized(_) => {
